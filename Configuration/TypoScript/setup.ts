5b9d5940a34841a88a264e401c5de122540f7ba7
Fabien Udriot <fabien.udriot@ecodev.ch>
2015-11-23 11:37:38 +0100

    [FEATURE] new way of loading assets
    Resolves #8

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -1,6 +1,7 @@
-###############################################################
-# Extbase configuration
-###############################################################
+#################################
+# plugin.tx_infinitescrollgallery
+#################################
+
 plugin.tx_infinitescrollgallery {
 	view {
 		templateRootPath = {$plugin.tx_infinitescrollgallery.view.templateRootPath}
@@ -9,5 +10,29 @@ plugin.tx_infinitescrollgallery {
 	}
 	persistence {
 		storagePid = {$plugin.tx_infinitescrollgallery.persistence.storagePid}
+	}
+
+	settings {
+
+		asset {
+
+			vidiCss {
+				path = EXT:infinite_scroll_gallery/Resources/Public/StyleSheets/InfiniteScrollGallery.css
+				type = css
+
+				# Optional key if loading assets through EXT:vhs.
+				dependencies = mainCss
+			}
+
+			vidiJs {
+				path = EXT:infinite_scroll_gallery/Resources/Public/JavaScript/InfiniteScrollGallery.js
+				type = js
+
+				# Optional key if loading assets through EXT:vhs.
+				dependencies = mainJs
+			}
+		}
+
+		loadAssetWithVhsIfAvailable = 1
 	}
 }
