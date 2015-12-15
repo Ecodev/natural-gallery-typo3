7be2c9c63afe3be8ac97a9cfc1f260f7e7d0996a
Fabien Udriot <fabien.udriot@ecodev.ch>
2015-12-15 17:16:55 +0100

    [TASK] forgotten PSR-2 of TceForms
    Resolves #21

--- Classes/Backend/TceForms.php
+++ Classes/Backend/TceForms.php
@@ -24,109 +24,116 @@ use TYPO3\CMS\Extbase\Service\TypoScriptService;
 /**
  * A class to interact with TCEForms.
  */
-class TceForms {
+class TceForms
+{
 
 
-	/**
-	 * This method modifies the list of items for FlexForm "selection".
-	 *
-	 * @param array $parameters
-	 */
-	public function getSelections(&$parameters) {
+    /**
+     * This method modifies the list of items for FlexForm "selection".
+     *
+     * @param array $parameters
+     */
+    public function getSelections(&$parameters)
+    {
 
-		$parameters['items'][] = array('', '', NULL);
+        $parameters['items'][] = array('', '', NULL);
 
-		/** @var SelectionRepository $selectionRepository */
-		$selectionRepository = $this->getObjectManager()->get(SelectionRepository::class);
-		$selections = $selectionRepository->findForEveryone('sys_file');
+        /** @var SelectionRepository $selectionRepository */
+        $selectionRepository = $this->getObjectManager()->get(SelectionRepository::class);
+        $selections = $selectionRepository->findForEveryone('sys_file');
 
-		if ($selections) {
-			foreach($selections as $selection) {
-				/** @var Selection $selection */
-				$values = array($selection->getName(), $selection->getUid(), NULL);
-				$parameters['items'][] = $values;
-			}
-		}
-	}
+        if ($selections) {
+            foreach ($selections as $selection) {
+                /** @var Selection $selection */
+                $values = array($selection->getName(), $selection->getUid(), NULL);
+                $parameters['items'][] = $values;
+            }
+        }
+    }
 
-	/**
-	 * This method modifies the list of items for FlexForm "sorting".
-	 *
-	 * @param array $parameters
-	 */
-	public function feedItemsForSettingsSorting(&$parameters) {
-		$configuration = $this->getPluginConfiguration();
+    /**
+     * This method modifies the list of items for FlexForm "sorting".
+     *
+     * @param array $parameters
+     */
+    public function feedItemsForSettingsSorting(&$parameters)
+    {
+        $configuration = $this->getPluginConfiguration();
 
-		if (empty($configuration) || empty($configuration['settings']['templates'])) {
-			$parameters['items'][] = array('No template found. Forgotten to load the static TS template?', '', NULL);
-		} else {
+        if (empty($configuration) || empty($configuration['settings']['templates'])) {
+            $parameters['items'][] = array('No template found. Forgotten to load the static TS template?', '', NULL);
+        } else {
 
-			$configuredDataType = $this->getDataTypeFromFlexform($parameters);
+            $configuredDataType = $this->getDataTypeFromFlexform($parameters);
 
-			$parameters['items'][] = array('', '', NULL);
-			if (!empty($configuredDataType)) {
-				foreach(Tca::grid($configuredDataType)->getFields() as $fieldNameAndPath => $configuration) {
-					if (FALSE === strpos($fieldNameAndPath, '__')) {
-						$values = array($fieldNameAndPath, $fieldNameAndPath, NULL);
-						$parameters['items'][] = $values;
-					}
-				}
-			}
-		}
-	}
+            $parameters['items'][] = array('', '', NULL);
+            if (!empty($configuredDataType)) {
+                foreach (Tca::grid($configuredDataType)->getFields() as $fieldNameAndPath => $configuration) {
+                    if (FALSE === strpos($fieldNameAndPath, '__')) {
+                        $values = array($fieldNameAndPath, $fieldNameAndPath, NULL);
+                        $parameters['items'][] = $values;
+                    }
+                }
+            }
+        }
+    }
 
-	/**
-	 * @param $parameters
-	 * @return string
-	 */
-	protected function getDataTypeFromFlexform($parameters) {
+    /**
+     * @param $parameters
+     * @return string
+     */
+    protected function getDataTypeFromFlexform($parameters)
+    {
 
-		$configuredDataType = '';
-		if (!empty($parameters['row']['settings.dataType'][0])) {
-			$configuredDataType = $parameters['row']['settings.dataType'][0];
-		}
+        $configuredDataType = '';
+        if (!empty($parameters['row']['settings.dataType'][0])) {
+            $configuredDataType = $parameters['row']['settings.dataType'][0];
+        }
 
-		if (!empty($parameters['row']['pi_flexform'])) {
+        if (!empty($parameters['row']['pi_flexform'])) {
 
-			$flexform = GeneralUtility::xml2array($parameters['row']['pi_flexform']);
-			if (!empty($flexform['data']['general']['lDEF']['settings.dataType'])) {
-				$configuredDataType = $flexform['data']['general']['lDEF']['settings.dataType']['vDEF'];
-			}
-		}
-		return $configuredDataType;
-	}
+            $flexform = GeneralUtility::xml2array($parameters['row']['pi_flexform']);
+            if (!empty($flexform['data']['general']['lDEF']['settings.dataType'])) {
+                $configuredDataType = $flexform['data']['general']['lDEF']['settings.dataType']['vDEF'];
+            }
+        }
+        return $configuredDataType;
+    }
 
-	/**
-	 * Returns the TypoScript configuration for this extension.
-	 *
-	 * @return array
-	 */
-	protected function getPluginConfiguration() {
-		$setup = $this->getConfigurationManager()->getTypoScriptSetup();
+    /**
+     * Returns the TypoScript configuration for this extension.
+     *
+     * @return array
+     */
+    protected function getPluginConfiguration()
+    {
+        $setup = $this->getConfigurationManager()->getTypoScriptSetup();
 
-		$pluginConfiguration = array();
-		if (is_array($setup['plugin.']['tx_infinitescrollgallery.'])) {
+        $pluginConfiguration = array();
+        if (is_array($setup['plugin.']['tx_infinitescrollgallery.'])) {
 
-			/** @var TypoScriptService $typoScriptService */
-			$typoScriptService = GeneralUtility::makeInstance(TypoScriptService::class);
-			$pluginConfiguration = $typoScriptService->convertTypoScriptArrayToPlainArray($setup['plugin.']['tx_infinitescrollgallery.']);
-		}
-		return $pluginConfiguration;
-	}
+            /** @var TypoScriptService $typoScriptService */
+            $typoScriptService = GeneralUtility::makeInstance(TypoScriptService::class);
+            $pluginConfiguration = $typoScriptService->convertTypoScriptArrayToPlainArray($setup['plugin.']['tx_infinitescrollgallery.']);
+        }
+        return $pluginConfiguration;
+    }
 
-	/**
-	 * @return BackendConfigurationManager
-	 */
-	protected function getConfigurationManager() {
-		return $this->getObjectManager()->get(BackendConfigurationManager::class);
-	}
+    /**
+     * @return BackendConfigurationManager
+     */
+    protected function getConfigurationManager()
+    {
+        return $this->getObjectManager()->get(BackendConfigurationManager::class);
+    }
 
-	/**
-	 * @return ObjectManager
-	 */
-	protected function getObjectManager() {
-		/** @var ObjectManager $objectManager */
-		return GeneralUtility::makeInstance(ObjectManager::class);
-	}
+    /**
+     * @return ObjectManager
+     */
+    protected function getObjectManager()
+    {
+        /** @var ObjectManager $objectManager */
+        return GeneralUtility::makeInstance(ObjectManager::class);
+    }
 
 }
