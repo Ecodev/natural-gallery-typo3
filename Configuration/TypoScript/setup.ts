94d41b8e9a7cef9b3d50671f3eb42a198ae56314
Samuel Baptista <samuel.baptista@ecodev.ch>
2016-02-29 20:49:54 +0100

    Update to natural-gallery with his new features
    New features of natural-gallery 1.4 : theming, show labels, show header and filters options

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -17,28 +17,33 @@ plugin.tx_naturalgallery {
 		asset {
 
 			photoSwipeCss {
-				path = EXT:natural_gallery/Resources/Public/StyleSheets/photoswipe.css
+				path = EXT:natural_gallery/Resources/Public/StyleSheets/photoswipe/photoswipe.css
 				type = css
 
 				# Optional key if loading assets through EXT:vhs.
 				dependencies = mainCss
 			}
 			photoSwipeThemeCss {
-				path = EXT:natural_gallery/Resources/Public/StyleSheets/default-skin/default-skin.css
+				path = EXT:natural_gallery/Resources/Public/StyleSheets/photoswipe/default-skin/default-skin.css
 				type = css
 
 				# Optional key if loading assets through EXT:vhs.
 				dependencies = mainCss
 			}
+			naturalGalleryThemeCss {
+				path = EXT:natural_gallery/Resources/Public/StyleSheets/natural-gallery/themes/natural.css
+				type = css
 
+				# Optional key if loading assets through EXT:vhs.
+				dependencies = mainCss
+			}
 			naturalGalleryCss {
-				path = EXT:natural_gallery/Resources/Public/StyleSheets/natural-gallery.css
+				path = EXT:natural_gallery/Resources/Public/StyleSheets/natural-gallery/natural-gallery.min.css
 				type = css
 
 				# Optional key if loading assets through EXT:vhs.
 				dependencies = mainCss
 			}
-
 			photoSwipeJs {
 				path = EXT:natural_gallery/Resources/Public/JavaScript/photoswipe.min.js
 				type = js
@@ -53,18 +58,8 @@ plugin.tx_naturalgallery {
 				# Optional key if loading assets through EXT:vhs.
 				dependencies = mainJs
 			}
-
-			organizer {
-				path = EXT:natural_gallery/Resources/Public/JavaScript/natural-gallery-organizer.js
-				type = js
-
-				# Optional key if loading assets through EXT:vhs.
-				dependencies = mainJs
-			}
-
-
 			naturalGalleryJs {
-				path = EXT:natural_gallery/Resources/Public/JavaScript/natural-gallery.js
+				path = EXT:natural_gallery/Resources/Public/JavaScript/natural-gallery.min.js
 				type = js
 
 				# Optional key if loading assets through EXT:vhs.
