de467874552868faf6dac0329feabcba58aa8948
Fabien Udriot <fabien@omic.ch>
2019-04-06 17:20:05 +0200

    Fix BE wizard

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,16 +1,48 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-    die ('Access denied.');
-}
+defined('TYPO3_MODE') or die();
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'Fab.natural_gallery',
-    'Pi1',
-    array(
-        'Gallery' => 'list',
-    ),
-    // non-cachable actions
-    array(
-        #'Gallery' => 'list',
-    )
+call_user_func(
+    function () {
+
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+            'Fab.natural_gallery',
+            'Pi1',
+            array(
+                'Gallery' => 'list',
+            ),
+            // non-cachable actions
+            array(
+                #'Gallery' => 'list',
+            )
+        );
+
+        // Register icons
+        $icons = [
+            'content-natural-gallery' => 'EXT:natural_gallery/Resources/Public/Images/NaturalGallery.png',
+        ];
+        $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+        foreach ($icons as $identifier => $path) {
+            $iconRegistry->registerIcon(
+                $identifier, TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class, ['source' => $path]
+            );
+        }
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+            'mod {
+                wizards.newContentElement.wizardItems.plugins {
+                    elements {
+                        tx_naturalgallery_templatebasedcontent {
+                            iconIdentifier = content-natural-gallery
+                            title = LLL:EXT:natural_gallery/Resources/Private/Language/locallang.xlf:wizard.title
+                            description = LLL:EXT:natural_gallery/Resources/Private/Language/locallang.xlf:wizard.description
+                            tt_content_defValues {
+                                CType = list
+                                list_type = naturalgallery_pi1
+                            }
+                        }
+                    }
+                }
+            }'
+        );
+    }
 );
--- ext_tables.php
+++ ext_tables.php
@@ -15,7 +15,3 @@ $pluginSignature = strtolower($extensionName) . '_pi1';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,recursive,select_key,pages';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:natural_gallery/Configuration/FlexForm/NaturalGallery.xml');
-
-if (TYPO3_MODE === "BE") {
-	$TBE_MODULES_EXT["xMOD_db_new_content_el"]['addElClasses']['tx_naturalgallery_wizard'] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('natural_gallery') . 'Classes/Backend/Wizard.php';
-}
