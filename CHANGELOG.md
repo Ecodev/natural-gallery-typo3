5b6b00f803a9218c6a2785a5ee8ff3bf6833c675
Fabien Udriot <fabien@omic.ch>
2022-06-17 09:16:03 +0200

    Release 3.0.0 - <https://github.com/Ecodev/natural_gallery/compare/2.2.0...3.0.0>

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+## 3.0.0 (2022-06-17)
+
+[FIX] TYPO3 compatibility
+[REFACTOR] TYPO3 v11 compatibility
+[TASK] Prepare next release
+
+
+
--- ext_emconf.php
+++ ext_emconf.php
@@ -3,25 +3,9 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Natural Gallery',
     'description' => 'A lazy load, infinite scroll and natural layout list gallery',
-    'category' => 'plugin',
     'author' => 'Fabien Udriot, Sylvain Tissot, Samuel Baptista',
     'author_email' => 'fabien.udriot@ecodev.ch, sylvain.tissot@ecodev.ch, samuel.baptista@ecodev.ch',
     'author_company' => 'Ecodev',
     'state' => 'stable',
-    'version' => '2.3.0-dev',
-    'constraints' =>
-        [
-            'depends' =>
-                [
-                    'typo3' => '10.0.0-10.4.99',
-                    'vidi' => '5.0.0-0.0.0',
-                    'vhs' => '0.0.0-0.0.0',
-                ],
-            'conflicts' =>
-                [
-                ],
-            'suggests' =>
-                [
-                ],
-        ],
 ];
+
