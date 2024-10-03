3791a5a73552e272687c62f460db89fd6a81a5a1
Lebeau Valangui <lebeau@declic.africa>
2024-10-03 15:49:41 +0000

    fix: configuration imageGalleryRepository

--- Classes/Domain/Repository/ImageGalleryRepository.php
+++ Classes/Domain/Repository/ImageGalleryRepository.php
@@ -1,23 +1,81 @@
 <?php
 namespace Fab\NaturalGallery\Domain\Repository;
 
-/**
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
- */
 
-/**
- * Repository for querying content element
- */
+use Fab\NaturalGallery\Utility\ConfigurationUtility;
+
+
+
 class ImageGalleryRepository 
 {
-    
+
+    public function getDefaultData(string $field):string 
+    {
+        return ConfigurationUtility::getInstance()->get($field);
+    }
+
+    public function findByUid(int $uid): array
+    {
+        $query = $this->getQueryBuilder();
+        $query
+            ->select('*')
+            ->from($this->tableName)
+            ->where(
+                $this->getQueryBuilder()
+                    ->expr()
+                    ->eq('uid', $this->getQueryBuilder()->expr()->literal($uid)),
+            );
+
+        $messages = $query->execute()->fetchOne();
+
+        return is_array($messages) ? $messages : [];
+    }
+
+    /**
+     * @throws Exception
+     * @throws DBALException
+     */
+    public function findByDemand(array $demand = [], array $orderings = [], int $offset = 0, int $limit = 0): array
+    {
+        $queryBuilder = $this->getQueryBuilder();
+        $queryBuilder->select('*')->from($this->tableName);
+
+        $constraints = [];
+        foreach ($demand as $field => $value) {
+            $constraints[] = $queryBuilder
+                ->expr()
+                ->like(
+                    $field,
+                    $queryBuilder->createNamedParameter('%' . $queryBuilder->escapeLikeWildcards($value) . '%'),
+                );
+        }
+        if ($constraints) {
+            $queryBuilder->where($queryBuilder->expr()->orX(...$constraints));
+        }
+
+        # We handle the sorting
+        $queryBuilder->addOrderBy(key($orderings), current($orderings));
+
+        if ($limit > 0) {
+            $queryBuilder->setMaxResults($limit);
+        }
+
+        return $queryBuilder->execute()->fetchAllAssociative();
+    }
+
+    /**
+     * @throws DBALException
+     * @throws Exception
+     */
+    public function findByUids(array $uids): array
+    {
+        $query = $this->getQueryBuilder();
+        $query
+            ->select('*')
+            ->from($this->tableName)
+            ->where($this->getQueryBuilder()->expr()->in('uid', $uids));
+
+        return $query->execute()->fetchAllAssociative();
+    }
+
 }
